ae911fb3958935dfdf86bbaf02083f813464859d
Qajay0832 <123375564+Qajay0832@users.noreply.github.com>
2024-06-06 13:11:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //your code here!
-
+let totalList=10;
+let list=document.getElementById("infi-list")
+window.addEventListener('load',()=>{
+    for(let i=1;i<=totalList;i++){
+        let li=document.createElement('li');
+        li.innerText=`Item ${i}`;
+        list.append(li);
+        }
+});
+list.addEventListener("scroll",()=>{
+    for(let i=0;i<=2;i++){
+        let li=document.createElement('li');
+        li.innerText=`Item ${++totalList}`;
+        list.append(li);
+        }   
+})
